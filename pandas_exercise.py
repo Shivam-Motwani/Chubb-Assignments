9173e35c32e56920f3d6de8f9a4912f8fc39979f
Nine Heavens <theoryofnumbers123@gmail.com>
2025-11-17 23:29:51 +0530

    Question 2

--- pandas_exercise.py
+++ pandas_exercise.py
@@ -9,3 +9,14 @@ df["date"] = pd.to_datetime(df["date"])  #date to datetime
 
 df["promo_code"].fillna("NO_PROMO", inplace=True) #missiing values
 df["customer_id"].fillna(-1, inplace=True)
+
+#2 Filter and subset rows/columns (e.g., sales for a store, date ranges, high-value transactions).
+#Sales only from Downtown store
+downtown_sales = df[df["store_name"] == "Downtown"]
+print(downtown_sales)
+#Transactions after October 3rd
+after_3 = df[df["date"] > "2025-10-03"]
+print(after_3)
+#High-value transactions ( > Rs.5)
+high_value = df[df["total"] > 5]
+print(high_value)
